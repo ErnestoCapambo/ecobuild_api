e492a0b921be2ef683a53a33db94a0ca4629744b
ErnestoCapambo <ernestosikilitacapambo@gmail.com>
2024-11-05 01:14:33 +0100

    _

--- src/Config/App.ts
+++ src/Config/App.ts
@@ -51,7 +51,7 @@ export class Application {
     middlewares() {
 
         this.app.use(cors({
-            origin: "*", // Permite origens
+            origin: ['https://ecobuild.onrender.com', 'http://localhost:5173'], // Permite origens
             methods: ["GET", "POST", "PUT", "DELETE"], // Métodos permitidos
             allowedHeaders: ["Content-Type", "Authorization"], // Headers permitidos
             // credentials: true
